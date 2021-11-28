6b0b2c1d67af443877b6c1449411b4a19a2e8d0f
josserden <d-h11@ukr.net>
2021-11-28 12:17:47 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -7,30 +7,67 @@ import { renderMarkup } from './js/renderMarkup';
 import refs from './js/refs';
 import card from './template/card.hbs';
 
-const onSearch = event => {
-  event.preventDefault();
-  refs.gallery.innerHTML = '';
-  ApiService.resetPage();
+const showSpinner = {
+  visible() {
+    refs.loader.classList.remove('d-none');
+  },
 
-  const userRequest = event.currentTarget.elements.query.value.trim();
+  hidden() {
+    refs.loader.classList.add('d-none');
+  },
+};
 
-  if (!userRequest) return swal('Bad request!', 'You input empty!', 'error');
+let options = {
+  root: null,
+  rootMargin: '10px 0px',
+  threshold: 0.25,
+};
 
-  ApiService.query = userRequest;
+const intersected = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) fetchImg();
+  });
+};
 
+let observer = new IntersectionObserver(intersected, options);
+
+function fetchImg() {
   ApiService.fetchImage().then(data => {
-    if (!data) return;
-
     const options = {
       ref: refs.gallery,
       template: card,
       data: data.hits,
     };
 
+    if (data.hits.length === 0) {
+      showSpinner.hidden();
+      return swal('Ooops!', 'Image not found!', 'error');
+    }
+
     renderMarkup(options);
   });
+}
+
+function onSearch(event) {
+  event.preventDefault();
+  refs.gallery.innerHTML = '';
+  ApiService.resetPage();
+  showSpinner.visible();
+
+  const userRequest = event.currentTarget.elements.query.value.trim();
+
+  if (!userRequest) {
+    refs.loader.classList.add('d-none');
+    return swal('Bad request!', 'You input empty!', 'error');
+  }
+
+  ApiService.query = userRequest;
+  observer.observe(refs.loader);
 
   refs.form.reset();
-};
+}
+
+showSpinner.hidden();
+observer.unobserve(refs.loader);
 
 refs.form.addEventListener('submit', onSearch);
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export default {
   form: document.querySelector('.js-form'),
   gallery: document.querySelector('.js-gallery'),
+  loader: document.querySelector('#loader'),
 };
